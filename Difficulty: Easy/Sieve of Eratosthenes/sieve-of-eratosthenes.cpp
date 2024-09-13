22d91d358208106e007b9401b295ee4e1b269d9e
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-09-13 10:17:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sieve of Eratosthenes/sieve-of-eratosthenes.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<int> isprime(vector<int>v,int N)
+    {
+        for(int i=2;i*i<=N;i++){
+            if(v[i]==1){
+                for(int j=i*i;j<=N;j+=i){
+                    v[j]=0;
+                }
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<v.size();i++){
+            if(v[i]==1) ans.push_back(i);
+        }
+        return ans;
+    }
+    vector<int> sieveOfEratosthenes(int N)
+    {
+        // Write Your Code here
+        vector<int> v(N+1,1);
+        v[0]=0;
+        v[1]=0;
+        
+      
+        vector<int> ans=  isprime(v,N);
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        vector<int> primes  = ob.sieveOfEratosthenes(N);
+        for(auto prime : primes) {
+            cout<< prime <<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
