c479d37c9d1b094709befb5a32e9827a05f6cbb7
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-11-19 14:41:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int target) {
+        vector<bool> dp(target + 1, false);
+        dp[0] = true; // Base case: sum 0 is always possible
+    
+        for (int num : arr) {
+            for (int j = target; j >= num; --j) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+    
+        return dp[target];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
+
+        Solution ob;
+        if (ob.isSubsetSum(arr, sum))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
