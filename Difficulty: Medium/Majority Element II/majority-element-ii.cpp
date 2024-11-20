5675a15c69fe3f62ff3666017ebe14d470b0eee0
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-11-20 14:45:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element II/majority-element-ii.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the majority elements in the array
+    vector<int> findMajority(vector<int>& arr) {
+        // Your code goes here.
+        unordered_map<int,int> m;
+        for(auto it : arr){
+            m[it]++;
+        }
+        
+        int n = arr.size()/3;
+        
+        vector<int> v;
+        for(auto pair : m){
+            if(pair.second>n) v.push_back(pair.first);
+        }
+        
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<int> ans = ob.findMajority(nums);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto &i : ans)
+                cout << i << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
