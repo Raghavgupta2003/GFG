ef415d1d9c21b42a0e1d395e526673e56122f3c5
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-11-29 01:39:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Ways To Tile A Floor/ways-to-tile-a-floor.cpp
+++ Difficulty: Basic/Ways To Tile A Floor/ways-to-tile-a-floor.cpp
@@ -7,20 +7,14 @@ class Solution {
   public:
     int numberOfWays(int n) {
         //code here
-        // if(n<=2) return n;
-        // return numberOfWays(n-1)+numberOfWays(n-2);
-        
         if(n<=2) return n;
-        
-        int dp[n+1];
+        vector<int> dp(n+1,0);
         dp[1] = 1;
         dp[2] = 2;
-        for(int i=3;i<=n;i++){
-            dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
         }
-        
         return dp[n];
-        
     }
 };
 
