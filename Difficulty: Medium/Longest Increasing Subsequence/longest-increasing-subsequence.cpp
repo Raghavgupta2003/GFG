b868eb44c4fd27bcb346cbeca2597b3e05c84081
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-11-29 01:48:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -11,16 +11,13 @@ class Solution {
     // Function to find length of longest increasing subsequence.
     int longestSubsequence(vector<int>& arr) {
         // code here
-        int n= arr.size();
-        if(arr.size()==0) return 0;
-        int maxlength = 1;
+        int n = arr.size();
+        if(n==0) return 0;
         vector<int> dp(n,1);
-        
+        int maxlength = 1;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                if(arr[j]<arr[i]){
-                    dp[i] = max(dp[i],dp[j]+1);
-                }
+                if(arr[j]<arr[i])  dp[i] = max(dp[i],dp[j]+1);
             }
             maxlength = max(maxlength,dp[i]);
         }
