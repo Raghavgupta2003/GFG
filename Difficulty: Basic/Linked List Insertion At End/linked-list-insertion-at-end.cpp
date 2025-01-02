cb1e5bf2930060ce59a1db96549c606470c0d532
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-01-03 00:15:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.cpp
+++ Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.cpp
@@ -38,16 +38,15 @@ class Solution {
     Node *insertAtEnd(Node *head, int x) {
         // Code here
         Node* newnode = new Node(x);
-        if(head==NULL) {
+        if(head == NULL){
             head = newnode;
             return head;
         }
-        
-        Node* temp = head;
+        Node*temp = head;
         while(temp->next!=NULL){
-            temp=temp->next;
+            temp = temp->next;
         }
-        temp->next=newnode;
+        temp->next = newnode;
         
         return head;
     }
@@ -92,8 +91,8 @@ int main() {
         Solution ob;
         Node* ans = ob.insertAtEnd(head, x);
         printList(ans);
+        cout << "~\n";
     }
-
     return 0;
 }
 // } Driver Code Ends
