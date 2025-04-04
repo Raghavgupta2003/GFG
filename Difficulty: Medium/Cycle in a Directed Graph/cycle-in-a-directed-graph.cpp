4b0281f5470393b9eddfea87869651148fc0f019
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-04-05 03:17:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.cpp
+++ Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.cpp
@@ -7,41 +7,60 @@ using namespace std;
 
 class Solution {
   public:
-  
-    bool DFS(int node, vector<vector<int>> &adj, vector<int>& visited, vector<int>& pathvisited){
-        
-        visited[node] = 1;
-        pathvisited[node] = 1;
-        
-        for(auto it: adj[node]){
-            if(!visited[it]){
-                if(DFS(it, adj, visited, pathvisited)) return true;
-            }else{ 
-                //already visited but it should be on same path
-                if(pathvisited[it]) return true;
-            }
-        }
-        
-        pathvisited[node] = 0;
-        
-        return false;
-    }
     // Function to detect cycle in a directed graph.
-    bool isCyclic(vector<vector<int>> &adj) {
+    string isCyclic(vector<vector<int>> &adj) {
         // code here
-        
-        int n = adj.size(); // number of nodes
-        
-        vector<int> visited(n, 0);
-        vector<int> pathvisited(n, 0);
+        // int n = 0; //number of nodes
+        // set<int> s;
+        // for(int i=0; i<edges.size(); i++){
+        //     s.insert(edges[i][0]);
+        //     s.insert(edges[i][1]);
+        // }
+        // n = s.size();
+        // //adj list
+        // vector<vector<int>> adj(n, vector<int> (0));
+        // for(int i=0; i<edges.size(); i++){
+        //     int a = edges[i][0];
+        //     int b = edges[i][1];
+            
+        //     adj[a].push_back(b);
+        // }
+         
+        int n = adj.size();
+        // Now working on khans algorithm
+        vector<int> indegree(n);
         
         for(int i=0; i<n; i++){
-            if(!visited[i]){
-                if(DFS(i, adj, visited, pathvisited)) return true;
+            for(auto it : adj[i]){
+                indegree[it]++;
             }
         }
         
-        return false;
+        queue<int> q;
+        
+        //pushing element whose indegree is 0;
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int> toposort;
+        
+        //applying bfs
+        while(q.size() > 0){
+            int cur = q.front();
+            
+            q.pop();
+            toposort.push_back(cur);
+            
+            for(auto it : adj[cur]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        
+        // if toposort valid then no cycle
+        if(toposort.size() == n) return "false";
+        
+        return "true";
     }
 };
 
