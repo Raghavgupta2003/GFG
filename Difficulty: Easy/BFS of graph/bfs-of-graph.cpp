39bb4090fcea436a0726caa60f0bc594ced3c845
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-23 13:05:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,77 +1,28 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
     vector<int> bfs(vector<vector<int>> &adj) {
         // Code here
-        int n = adj.size(); // NUMBER OF NODES
+        vector<int> ans;
+        int n = adj.size();
+        
         vector<int> visited(n, 0);
         
         queue<int> q;
-        q.push(0);
+        q.push(0); //node zero
         visited[0] = 1;
-        
-        vector<int> v;
-        
         while(q.size() > 0){
-            int cur = q.front();
-            
+            int node = q.front();
+            ans.push_back(node);
             q.pop();
-            
-            for(auto it : adj[cur]){
-                if(!visited[it]){
+            for(auto it : adj[node]){
+                if(visited[it] == 0){
                     q.push(it);
                     visited[it] = 1;
                 }
             }
-            v.push_back(cur);
         }
+        return ans;
         
-        return v;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore();
-    while (tc--) {
-        int V;
-        cin >> V;
-        cin.ignore();
-        // Use vector of vectors instead of array of vectors.
-        vector<vector<int>> adj(V);
-
-        for (int i = 0; i < V; i++) {
-            string input;
-            getline(cin, input);
-            int num;
-            vector<int> node;
-            stringstream ss(input);
-            while (ss >> num) {
-                node.push_back(num);
-            }
-            adj[i] = node;
-        }
-
-        Solution obj;
-        vector<int> ans = obj.bfs(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
