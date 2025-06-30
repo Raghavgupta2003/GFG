d5727832a37a633801ef9a4d450aeb0bc5bf8205
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-30 10:15:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
@@ -1,65 +1,31 @@
-//{ Driver Code Starts
-//Initial Template for C++
+// User function Template for C++
 
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-public:
-    void insertAtBottom(int ele,stack<int> &st){
-        if(st.size()==0){
-            st.push(ele);
-            return;
+class Solution {
+  public:
+    void pushAtLast(int x, stack<int> &st){
+        stack<int> temp;
+        while(st.size()>0){
+            temp.push(st.top());
+            st.pop();
         }
-        int Top = st.top();
-        st.pop();
-        insertAtBottom(ele,st);
-        st.push(Top);
-    }
-    void Reverse(stack<int> &st){
-        if(st.size()==0) return;
-        int Top = st.top();
-        st.pop();
-        Reverse(st);
-        insertAtBottom(Top,st);
-    }
-};
-
-//{ Driver Code Starts.
-
-
-int main(){
-    int T;
-    cin>>T;
-    while(T--){
-        int N;
-        cin>>N;
-        stack<int> St;
-        for(int i=0;i<N;i++){
-            int x;
-            cin>>x;
-            St.push(x);
-        }
-        Solution ob;
-        ob.Reverse(St);
-        vector<int>res;
-        while(St.size())
-        {
-            res.push_back(St.top());
-            St.pop();
-        }
-        for(int i = res.size()-1;i>=0;i--)
-        {
-            cout<<res[i]<<" ";
+        //after emplting stack, push it in stack
+        st.push(x);
+        //push remaining ele in stack
+        while(temp.size()>0){
+            st.push(temp.top());
+            temp.pop();
         }
         
-        cout<<endl;
-    
-cout << "~" << "\n";
-}
-}
-// } Driver Code Ends
+    }
+    void rev(int i, int n, stack<int> &St){
+        if(i >= n) return;
+        int x = St.top();
+        St.pop();
+        rev(i+1, n, St);
+        pushAtLast(x, St);
+    }
+    void Reverse(stack<int> &St) {
+        int n = St.size();
+        rev(0, n, St);
+    }
+};
