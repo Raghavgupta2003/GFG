efcd0c3c23cd27821f9001acf1368b94ad4c6c75
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-05 03:53:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,41 +1,14 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// class Solution {
-//   public:
-//     int cost(int idx, vector<int>& heights){
-//         if(idx == 0) return 0;
-        
-//         int left = cost(idx-1, heights) + abs(heights[idx] - heights[idx-1]);
-//         int right = INT_MAX;
-//         if(idx>1) right = cost(idx-2, heights) + abs(heights[idx] - heights[idx-2]); 
-        
-//         return min(left, right);
-//     }
-//     int minCost(vector<int>& height) {
-//         // Code here
-//         int n = height.size();
-//         return cost(n-1, height);
-//     }
-// };
-
-//----------memorization-----------
-
 class Solution {
   public:
-    int cost(int idx, vector<int>& heights, vector<int>& dp){
-        if(idx == 0) return 0;
+    int cost(int n, vector<int>& height, vector<int>& dp){
+        if(n<=0) return 0; //jump from 0 to 0
         
-        if(dp[idx] !=-1) return dp[idx];
-        int left = cost(idx-1, heights,dp) + abs(heights[idx] - heights[idx-1]);
-        int right = INT_MAX;
-        if(idx>1) right = cost(idx-2, heights,dp) + abs(heights[idx] - heights[idx-2]); 
+        if(dp[n] != -1) return dp[n];
+        int skip1 = abs(height[n] - height[n-1]) + cost(n-1, height, dp);
+        int skip2 = 1e9;
+        if(n > 1) skip2 = abs(height[n] - height[n-2]) + cost(n-2, height, dp);
         
-        return dp[idx] = min(left, right);
+        return dp[n] = min(skip1, skip2);
     }
     int minCost(vector<int>& height) {
         // Code here
@@ -44,28 +17,3 @@ class Solution {
         return cost(n-1, height, dp);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    string str;
-    getline(cin, str);
-    int t = stoi(str);
-    while (t--) {
-        getline(cin, str);
-        stringstream ss(str);
-        vector<int> arr;
-        int num;
-        while (ss >> num) {
-            arr.push_back(num);
-        }
-        Solution ob;
-        cout << ob.minCost(arr) << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
