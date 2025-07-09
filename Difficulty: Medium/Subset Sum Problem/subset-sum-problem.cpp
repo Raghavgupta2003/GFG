cc79cb7dab0b73af00ed4d8e17aa1b708b6cdf7f
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-09 11:15:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,100 +1,26 @@
-//{ Driver Code Starts
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
-// class Solution {
-//   public:
-//     bool isexist(int idx, vector<int>& arr, int target, vector<vector<int>>& dp){
-       
-//         if(target == 0) return true;
-//          if(idx<0) return false;
-//         //  if(idx == 0) return arr[idx] == target;
-        
-//         if(dp[idx][target]!= -1) return dp[idx][target];
-//         bool pick = false;
-//         if(arr[idx]<=target) pick = isexist(idx-1, arr, target-arr[idx], dp);
-        
-//         bool notpick = isexist(idx-1, arr, target, dp);
-        
-//         return dp[idx][target] = (pick || notpick);
-//     }
-//     bool isSubsetSum(vector<int>& arr, int target) {
-//         // code here
-//         int n = arr.size();
-//         int idx = n-1;
-//         // vector<vector<int>> dp(200+1, vector<int>(4*10000 + 1, -1));
-//          vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-//         return isexist(idx, arr, target, dp);
-//     }
-// };
-
 class Solution {
   public:
-    bool isSubsetSum(vector<int>& arr, int target) {
+    bool ans(int i, vector<int>& arr, int sum, vector<vector<int>>& dp){
+        if(sum == 0) return true;
+        if(i == 0) return arr[0] == sum;
+        
+        if(dp[i][sum] != -1) return dp[i][sum];
+        
+        bool pick = false;
+        if(sum >= arr[i]) pick = ans(i-1, arr, sum-arr[i], dp);
+        
+        bool notpick = ans(i-1, arr, sum, dp);
+        
+        return dp[i][sum] =  pick || notpick;
+    }
+    bool isSubsetSum(vector<int>& arr, int sum) {
         // code here
+        
         int n = arr.size();
-        vector<vector<bool>> dp(n, vector<bool>(target + 1, 0));
         
-        //basecase
-        for(int i=0; i<arr.size(); i++){
-            dp[i][0] = true;
-        }
-        if(arr[0] <= target) dp[0][arr[0]] = true;
+        vector<vector<int>> dp(n, vector<int>(sum+1, -1));
         
-        //dp
-        
-        for(int i=1; i<n; i++){
-            
-            for(int j=1; j<=target; j++){
-                bool pick = false;
-                if(arr[i]<=j) pick = dp[i-1][j-arr[i]];
-                
-                bool notpick = dp[i-1][j];
-                
-                dp[i][j] = pick || notpick;
-            }
-        }
-        
-        return dp[n-1][target];
+        return ans(n-1, arr, sum , dp);
         
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        int sum;
-        cin >> sum;
-        cin.ignore();
-
-        Solution ob;
-        if (ob.isSubsetSum(arr, sum))
-            cout << "true" << endl;
-        else
-            cout << "false" << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
