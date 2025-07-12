fdacb33b1e63f4b049b2f1beb5d82f732b345471
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-12 06:34:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -1,47 +1,24 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 
 class Solution {
   public:
     bool isBalanced(string& s) {
         // code here
         stack<int> st;
+        
         for(int i=0; i<s.size(); i++){
-            char ch = s[i];
-            if(st.size()>0){
-                if(ch == ')' && st.top() == '(') st.pop();
-                else if(ch == '}' && st.top() == '{') st.pop();
-                else if(ch == ']' && st.top() == '[') st.pop();
-                else st.push(ch);
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
             }
-            else st.push(ch);
+            else if(st.size() > 0){
+                if(s[i] == ')' && st.top()=='(') st.pop();
+                else if(s[i] == '}' && st.top()=='{') st.pop();
+                else if(s[i] == ']' && st.top()=='[') st.pop();
+                else st.push(s[i]);
+            }
+            else st.push(s[i]);
         }
         
-        if(st.size() == 0) return true;
-        return false;
+        return st.size() == 0;
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    string a;
-    cin >> t;
-    while (t--) {
-        cin >> a;
-        Solution obj;
-        if (obj.isBalanced(a))
-            cout << "true" << endl;
-        else
-            cout << "false" << endl;
-
-        cout << "~"
-             << "\n";
-    }
-}
-// } Driver Code Ends
